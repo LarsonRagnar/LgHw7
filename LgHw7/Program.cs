83150ed41858ea7e4151a829661b15e61b3428eb
LarsonRagnar <larionov1992@gmail.com>
2023-03-08 21:06:19 +0300

    изменил 1 задачу добавил ввод m,n

--- LgHw7/Program.cs
+++ LgHw7/Program.cs
@@ -9,35 +9,40 @@
 
 // 8 7,8 -7,1 9
 
-// double[,] Getmatrix(int rows, int cols)
-// {
-//     double[,] matrix = new double[rows, cols];
-//     for (int i = 0; i < matrix.GetLength(0); i++)  
-//     {
-//         for (int j = 0; j < matrix.GetLength(1); j++) 
-//         {
-//             matrix[i, j] = new Random().NextDouble()*100;
-//         }
+Console.WriteLine("Введите количество строк: m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество строк: n");
+int n = Convert.ToInt32(Console.ReadLine());
 
-//     }
-//     return matrix;
-// }
-// void Printmatrix(double [,] matr)    
-// {
-//     for (int i = 0; i < matr.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < matr.GetLength(1); j++)
-//         {
-//             Console.Write(matr[i,j] + "\t");
-//         }
-//         Console.WriteLine();
-//     }
-// }
+double[,] Getmatrix(int rows, int cols)
+{
+    double[,] matrix = new double[rows, cols];
+    for (int i = 0; i < matrix.GetLength(0); i++)  
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            matrix[i, j] = new Random().NextDouble()*100;
+        }
+
+    }
+    return matrix;
+}
+void Printmatrix(double [,] matr)    
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
 
 
-// double[,] resultmatrix = Getmatrix(3,4);   
+double[,] resultmatrix = Getmatrix(m,n);   
 
-// Printmatrix(resultmatrix);
+Printmatrix(resultmatrix);
 
 
 
